9c8896a34e16ad592b6f1f392d925555de7f6531
Divyansh Singh <109480881+Divyansh-Singh-Rathore@users.noreply.github.com>
2022-09-01 04:04:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+//--------------------      For Product Ordering Page to change images when clicked      ----------------------//
+
+function changeImage(event){
+    document.querySelector(".prodImage").src = event.children[0].src
+} 
+
